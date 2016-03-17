b9a83da71235df2e48d1d18ce4a241ecb68dd7b1
michaelmob <4530010+michaelmob@users.noreply.github.com>
2016-03-17 14:39:53 -0400

    Remove postback url

--- project/apps/conversions/urls.py
+++ project/apps/conversions/urls.py
@@ -5,8 +5,5 @@ from .views import notifications
 from .models import Deposit
 
 urlpatterns = [
-	url(r"^postback-send/$", postback.internal, name="postback-send"),
-	url(r"^postback-receive/(?P<password>\w+)/$", postback.receive, name="postback-receive"),
-
 	url(r"^notify/(?P<password>\w+)/$", notifications.receive, name="http-notification-receive"),
 ]
