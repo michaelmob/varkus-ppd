3691e4ff5de48aa431a8ebbcf2390a2a7fa26d11
michaelmob <4530010+michaelmob@users.noreply.github.com>
2017-02-09 01:31:12 -0500

    Correct template location for password reset confirmation page

--- project/apps/users/urls.py
+++ project/apps/users/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
 	url(r"^password_change/done/$", auth_views.password_change_done, {"template_name": "password/password_change_done.html"}, name="password_change_done"),
 	url(r"^password_reset/$", auth_views.password_reset, {"template_name": "password/password_reset.html"}, name="password_reset"),
 	url(r"^password_reset/done/$", auth_views.password_reset_done, {"template_name": "password/password_reset_done.html"}, name="password_reset_done"),
-	url(r"^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$", auth_views.password_reset_confirm, {"template_name": "users/password/password_reset_confirm.html"}, name="password_reset_confirm"),
+	url(r"^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$", auth_views.password_reset_confirm, {"template_name": "password/password_reset_confirm.html"}, name="password_reset_confirm"),
 	url(r"^reset/done/$", auth_views.password_change, {"template_name": "password/password_reset_complete.html"}, name="password_reset_complete")
 ]
